3543888a939bb46ce16a667a50ac6e30daff645f
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-19 20:49:05 +0200

    implement BaseRobot class generator

--- app/main.py
+++ app/main.py
@@ -2,4 +2,8 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+class BaseRobot:
+    def __init__(self, name: str, weight: int, coords: list[int] = [0, 0]) -> None:
+        self.name = name
+        self.weight = weight
+        self.coords = coords
