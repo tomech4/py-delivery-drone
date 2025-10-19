b1e3683e83b629d0d8a4697a6afee1cde9ba985c
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-19 22:24:22 +0200

    create unhook_load for DeliveryDrone

--- app/main.py
+++ app/main.py
@@ -53,3 +53,6 @@ class DeliveryDrone(FlyingRobot):
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight < self.max_load_weight:
             self.current_load = cargo
+
+    def unhook_load(self, cargo: Cargo) -> None:
+        self.current_load = None
