5131bbd87e625e304f34685468ad03406c902bf3
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-19 22:01:39 +0200

    create movement methods for FlyingRobot

--- app/main.py
+++ app/main.py
@@ -30,3 +30,9 @@ class FlyingRobot(BaseRobot):
             self.coords.append(0)
 
         super().__init__(name, weight, coords)
+
+    def go_up(self, step: int = 1) -> None:
+        self.coords[2] += step
+
+    def go_down(self, step: int = 1) -> None:
+        self.coords[2] -= step
